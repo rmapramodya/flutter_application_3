75e4e4f491518bc60accebdc5d42183bdfd95397
rmapramodya <96515463+rmapramodya@users.noreply.github.com>
2023-07-13 15:30:09 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/HomePage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
